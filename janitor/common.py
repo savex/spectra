d05cac768556b61915aaad6ea0152f22e9d9e713
Oleksandr Savatieiev <a.savex@gmail.com>
2018-02-01 14:50:51 +0200

    Moved cleaner to separate repo

--- janitor/common.py
+++
@@ -1,7 +0,0 @@
-import utils
-
-utils = utils
-logger, logger_cli = utils.logger.setup_loggers(
-    "janitor",
-    log_fname=utils.config.sweeper.get_logfile_path()
-)
--- janitor/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-import file
-import config
-import logger
-
-file_utils = file
-logger = logger
-config = config
